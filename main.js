cbe79c681b103a60f16de5495c6bf7300bd1b7cf
agtarrasa <fastdeliveries2020@gmail.com>
2021-09-27 17:16:25 +0000

    Generate vector of 3 items/0-1-2 randomly

--- main.js
+++ main.js
@@ -11,11 +11,24 @@ var opcionesTmp=[],indicesRandom=[];
 }
 
 function isRotated(){
-var cartasRotadas=
+var RotatedCards=
  $('div[class="card rotate"]').length;
- return cartasRotadas;   
+ return RotatedCards;   
 
 }
+function ceroA2Azar(){
+  var i=0;
+  var positions=[];
+  var RandomNumber=0;
+  while(positions.length!=3){
+  RandomNumber=Math.floor(Math.random()*3);
+  while(positions.includes(RandomNumber) ){
+  RandomNumber=Math.floor(Math.random()*3);
+  }
+  positions.push(RandomNumber);
+  }
+  return positions;
+  }
 
 
 
